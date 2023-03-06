5ba7a9203ddb529a4bab3967bf14597cd1c3256e
apu <apu.sust@gmail.com>
2023-03-06 20:47:34 +0600

    Initial commit

---
+++ src/main/java/demo/playground/reactivespringauth/ReactiveSpringAuthApplication.java
@@ -0,0 +1,13 @@
+package demo.playground.reactivespringauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveSpringAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveSpringAuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/demo/playground/reactivespringauth/ReactiveSpringAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.playground.reactivespringauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveSpringAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
